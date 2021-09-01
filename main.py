b3d2483ba656170624adb0612b1539fa82c136c9
Coder731 <stephenobrien1@gmail.com>
2021-09-01 18:48:55 +0000

    Add challenge code with work to date and error

---
+++ main.py
@@ -0,0 +1,71 @@
+class TicketMixin:
+    """Mixin to calculate ticket price based on age"""
+    def __init__(self, name, age):
+        self.name =name
+        self.age=age
+    def calculate_ticket_price(self, age):
+        if age<12:
+            price = 0
+        elif age<18:
+            price = 15
+        elif age<60:
+            price = 20
+        elif age>=60:
+            price=10
+        return price
+
+class Customer(TicketMixin):
+    """
+    Create instance of Customer
+    """
+    def __init__(self, name, age):
+        TicketMixin.__init__(self, name, age)
+        #instance attribute
+        self.name = name
+        self.age = age
+        
+    def describe(self):
+        print (f"{self.name} age {self.age} ticket price is {Customer(TicketMixin).calculate_ticket_price()}")
+
+customer = Customer("Ryan Phillips", 22)
+print(customer.describe())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# e_nd do not remove
